06f2d808c489fcc814bf48831705173a5fd09359
Martin Bachmann <martinpezbach@gmail.com>
2023-01-08 11:50:27 -0300

    test: add car register testes

---
+++ tests/unit/Services/carRegister.test.ts
@@ -0,0 +1,35 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { Model } from 'mongoose';
+
+describe('Rota de registro de carro', function () {
+  it('Registrando o carro com sucesso', async function () {
+    const carInput: ICar = {
+      model: 'Marea',
+      year: 2002,
+      color: 'Black',
+      status: true,
+      buyValue: 15.990,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
+
+    const carOutput: ICar = {
+      id: '6348513f34c397abcad040b2',
+      model: 'Marea',
+      year: 2002,
+      color: 'Black',
+      status: true,
+      buyValue: 15.990,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
+
+    sinon.stub(Model, 'create').resolves(carOutput);
+
+    const service = new CarService();
+    const result = await service.register(carInput);
+
+    expect(result).to.be.deep.equal(carOutput);
+  });
+});
